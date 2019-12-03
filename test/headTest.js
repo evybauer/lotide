109bec1b44d3ff9c8d2863506c7a488242d13735
Evelyn Bauer <evybauer@gmail.com>
2019-12-03 01:23:09 +0000

    describe and it functions headTest.js added

--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,29 @@
 
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([])); // => Should pass
-assertEqual(head([]), 0); // => Should not pass
-assertEqual(head([0]), 5); // => Should not pass
-assertEqual(head([0]), 0); // => Should pass
-assertEqual(head([5,6,7]), 5); // => Should pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // => Should pass
-assertEqual(head(["Lighthouse", "Labs"]), "Hello"); // => Should not pass
-assertEqual(head([]), "Hello"); // => Should not pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"])); // => Should not pass
+describe("#head", () => {
+  
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns '5' for ['5, 6, 7']", () => {
+    assert.strictEqual(head(['5', '6', '7']), '5'); 
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+
+  it("returns '[]' for ['[]']", () => {
+    assert.strictEqual(head(['[]']), '[]'); 
+  });
+
+});
+
+
