3d039597eecbee6acef86c9846c822c69b8b77e5
Evelyn Bauer <evybauer@gmail.com>
2019-12-03 02:00:59 +0000

    Updated file added

--- eqArrays.js
+++ eqArrays.js
@@ -3,11 +3,12 @@ const assertEqual = require('./assertEqual');
 const eqArrays = function(arr) {
   let arr1 = [];
   let arr2 = [];
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++ ){
     if (arr1.length === arr2.length)
-      return true;
+    return true;
   } if (Array.isArray(arr) && arr.length === 0)
     return true;
-};
+  }
 
 module.exports = eqArrays;
+
