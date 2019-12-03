dc319d36bd341b55d5acac8c15011e835f13c7ed
Evelyn Bauer <evybauer@gmail.com>
2019-12-03 00:27:55 +0000

    module eqArrays.js added

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,13 @@
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function(arr) {
   let arr1 = [];
   let arr2 = [];
-  for (let i = 0; i < arr.length; i++ ){
+  for (let i = 0; i < arr.length; i++) {
     if (arr1.length === arr2.length)
-    return true;
+      return true;
   } if (Array.isArray(arr) && arr.length === 0)
     return true;
-  }
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !== ${expected}`);
-  }
 };
 
-assertEqual(eqArrays([], []), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], ["1", 2, "3"]), false); // => should NOT PASS
-assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), false); // => should NOT PASS
-assertEqual(eqArrays(["Hello", "Lighthouse", "Labs"], ["Hello", "Lighthouse", "Labs"]), true); // => should PASS
-assertEqual(eqArrays(["Hello", "Lighthouse", "Labs"], ["Hello"]), false); // => should NOT PASS
-assertEqual(eqArrays(["Hello", "Lighthouse", "Labs"], [1, 2, 3]), false); // => should NOT PASS
-
-
+module.exports = eqArrays;
